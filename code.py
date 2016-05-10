ca5335749246dc381fe272459ae03b7bc87f515e
KobeIverson0 <KobeIverson0@users.noreply.github.com>
2016-05-10 10:48:56 +0800

    Update code.py

--- code.py
+++ code.py
@@ -4,10 +4,13 @@
 from math import sqrt
 
 def is_Prime(n):
-    for i in range(2, int(sqrt(n)) + 1):
-        if n % i == 0:
-            return False
-    return True
+    if n == 1 or n == 0:
+        return False
+    else:
+        for i in range(2, int(sqrt(n)) + 1):
+            if n % i == 0:
+                return False
+        return True
 
 count = 0
 res1 = 0
